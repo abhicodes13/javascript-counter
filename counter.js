d43369350541835fd5d363354dd439dfbbcbe389
abhicodes13 <68747686+abhicodes13@users.noreply.github.com>
2022-04-28 20:06:05 -0400

    Update counter.js

--- counter.js
+++ counter.js
@@ -5,7 +5,8 @@ function count() {
     counter++;
    document.querySelector('h1').innerHTML= counter;
    if (counter % 10 === 0){
-    alert(`Count is ${counter}`);
+   
+ alert(`Count is ${counter}`);
    }
  }
 
